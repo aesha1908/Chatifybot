8211d0ad8fc780e6ce54c3056cdb3bad100dd859
aesha1908 <aeshagandhi2003@gmail.com>
2023-09-26 23:15:16 +0530

    Translation

---
+++ lib/Translator/home.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController textEditingController = TextEditingController();
+  GoogleTranslator translator = GoogleTranslator();
+  var output;
+
+  void trans() {
+    translator
+        .translate(textEditingController.text, to: "hi")
+        .then((value) {
+      setState(() {
+        output = value;
+      });
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Chatify Translator"),
+        ),
+        body: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 20),
+              child: TextFormField(
+                style: const TextStyle(fontSize: 24),
+                controller: textEditingController,
+                onTap: () {
+                  trans();
+                },
+                decoration: const InputDecoration(
+                    labelText: 'Type Here',
+                    labelStyle: TextStyle(fontSize: 15)),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Text('Translated Text'),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              output == null ? "Please Select Language" : output.toString(),
+              style: const TextStyle(
+                  fontSize: 17,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w500),
+            ),
+          ],
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:newchatbot/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:newchatbot/Translator/home.dart';
 
 void main() async {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ChatScreen(),
+      home: Home(),
       debugShowCheckedModeBanner: false,
     );
   }
